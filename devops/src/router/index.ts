3f6a7da1121bc68d46c9b96c658ac55e9a22d8e2
hawk.wu <hawk.wu@oktech.sg>
2023-07-18 23:32:52 +0800

    第一次提交

---
+++ devops/src/router/index.ts
@@ -0,0 +1,12 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '/src/views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    { path: '/', name: 'home', component: HomeView},
+    {path: '/about', name: 'about', component: () => import('/src/views/AboutView.vue')}
+  ]
+})
+
+export default router
